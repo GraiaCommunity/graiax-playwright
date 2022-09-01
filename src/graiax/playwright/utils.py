93cb43b76a9c423aca54f0097f36e2a6cc3ddc00
I Love Study <1450069615@qq.com>
2022-09-02 01:38:44 +0800

    F, `kill` is not asynchronous

--- src/graiax/playwright/utils.py
+++ src/graiax/playwright/utils.py
@@ -8,17 +8,16 @@ from playwright._impl._driver import compute_driver_executable, get_driver_env
 
 
 async def install_playwright(download_host: Optional[str] = None):
-    select = re.compile(r"(\d+)%")
-    downloaded = re.compile(r"(?P<file>.*) downloaded to (?P<path>.*)")
+    select = re.compile("(\\d+)%")
+    downloaded = re.compile("(?P<file>.*) downloaded to (?P<path>.*)")
     last_progress_time = 0
 
     command = [str(compute_driver_executable()), "install", "chromium"]
     env = get_driver_env()
     if download_host:
         env["PLAYWRIGHT_DOWNLOAD_HOST"] = download_host
-    shell = await asyncio.create_subprocess_exec(
-        *command, stdout=asyncio.subprocess.PIPE, env=env
-    )
+    shell = await asyncio.create_subprocess_exec(*command, stdout=asyncio.subprocess.PIPE, env=env)
+    
     if shell.stdout is not None:
         first_line = (await shell.stdout.readline()).decode("UTF-8")
 
@@ -29,7 +28,7 @@ async def install_playwright(download_host: Optional[str] = None):
         elif "Downloading" not in first_line:
             logger.warning("Playwright 下载停止")
             logger.error(f"下载程序回报意外的内容:\n{first_line}")
-            await shell.kill()
+            shell.kill()
             return
 
         while line := (await shell.stdout.readline()).decode("UTF-8"):
@@ -47,7 +46,7 @@ async def install_playwright(download_host: Optional[str] = None):
                 logger.warning("Download Failed:\n" + message.decode("UTF-8"))
 
     if shell.returncode:
-        logger.warning(f"Playwright 下载失败，请尝试重新运行，或按照官方教程手动下载")
+        logger.warning("Playwright 下载失败，请尝试重新运行，或按照官方教程手动下载")
         logger.warning("Playwright 文档地址: https://playwright.dev/python/docs/intro")
         logger.warning("使用命令 poetry run playwright install 或 pdm run playwright install 可手动下载 Playwright")
     else:
