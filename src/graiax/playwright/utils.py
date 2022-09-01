54a29adb40c0347f5eb57ffe8ed3f41ea9e13af0
I Love Study <1450069615@qq.com>
2022-09-01 22:49:10 +0800

    Optimize detail(?)

--- src/graiax/playwright/utils.py
+++ src/graiax/playwright/utils.py
@@ -17,7 +17,7 @@ async def install_playwright(download_host: Optional[str] = None):
     if download_host:
         env["PLAYWRIGHT_DOWNLOAD_HOST"] = download_host
     shell = await asyncio.create_subprocess_exec(
-        *command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT, env=env
+        *command, stdout=asyncio.subprocess.PIPE, env=env
     )
     if shell.stdout is not None:
         first_line = (await shell.stdout.readline()).decode("UTF-8")
@@ -29,7 +29,7 @@ async def install_playwright(download_host: Optional[str] = None):
         elif "Downloading" not in first_line:
             logger.warning("Playwright 下载停止")
             logger.error(f"下载程序回报意外的内容:\n{first_line}")
-            shell.kill()
+            await shell.kill()
             return
 
         while line := (await shell.stdout.readline()).decode("UTF-8"):
@@ -37,10 +37,9 @@ async def install_playwright(download_host: Optional[str] = None):
             if percent and (time.time() - last_progress_time > 1 or percent[0] == "100"):
                 logger.info(f"Downloading {percent[0]}%")
                 last_progress_time = time.time()
-            elif "downloaded" in line:
-                if (p := downloaded.match(line)) is not None:
-                    p = p.groupdict()
-                    logger.info(f"{p['file']} 下载成功，下载位置：{p['path']}")
+            elif "downloaded" in line and (p := downloaded.match(line)) is not None:
+                p = p.groupdict()
+                logger.info(f"{p['file']} 下载成功，下载位置：{p['path']}")
             elif "Downloading" in line:
                 logger.info(f"Downloading {line[12:-1]}")
             elif line == "Failed to install browsers\n":
@@ -48,7 +47,7 @@ async def install_playwright(download_host: Optional[str] = None):
                 logger.warning("Download Failed:\n" + message.decode("UTF-8"))
 
     if shell.returncode:
-        logger.warning(f"Playwright 下载失败，退出代码【{shell.returncode}】，请尝试重新运行，或按照官方教程手动下载")
+        logger.warning(f"Playwright 下载失败，请尝试重新运行，或按照官方教程手动下载")
         logger.warning("Playwright 文档地址: https://playwright.dev/python/docs/intro")
         logger.warning("使用命令 poetry run playwright install 或 pdm run playwright install 可手动下载 Playwright")
     else:
