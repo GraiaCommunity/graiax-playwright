f29a7862506a760df7b37521040cf6662c57de5a
Redlnn <w731347477@gmail.com>
2022-09-01 21:36:23 +0800

    fix install_playwright

--- src/graiax/playwright/utils.py
+++ src/graiax/playwright/utils.py
@@ -1,11 +1,13 @@
-import re
 import asyncio
+import re
 import time
+from typing import Optional
 
 from loguru import logger
 from playwright._impl._driver import compute_driver_executable, get_driver_env
 
-async def install_playwright(download_host: str = None):   
+
+async def install_playwright(download_host: Optional[str] = None):
     select = re.compile(r"(\d+)%")
     downloaded = re.compile(r"(?P<file>.*) downloaded to (?P<path>.*)")
     last_progress_time = 0
@@ -14,36 +16,40 @@ async def install_playwright(download_host: str = None):
     env = get_driver_env()
     if download_host:
         env["PLAYWRIGHT_DOWNLOAD_HOST"] = download_host
-    shell = await asyncio.create_subprocess_exec(*command,
-                                                 stdout=asyncio.subprocess.PIPE,
-                                                 stderr=asyncio.subprocess.STDOUT,
-                                                 env=env)
-    
-    first_line = (await shell.stdout.readline()).decode('UTF-8')
+    shell = await asyncio.create_subprocess_exec(
+        *command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT, env=env
+    )
+    if shell.stdout is not None:
+        first_line = (await shell.stdout.readline()).decode('UTF-8')
 
-    if not first_line:
-        await shell.wait()
-        logger.info("已经下载了哦")
-        return
-    elif not "Downloading" in first_line:
-        logger.warning(f"wtf:\n{first_line}")
-        return
-    
-    while line := (await shell.stdout.readline()).decode('UTF-8'):
-        percent = select.findall(line)
-        if percent and (time.time() - last_progress_time > 1 or percent[0] == "100"):
-            logger.info(f"Downloading {percent[0]}%")
-            last_progress_time = time.time()
-        elif 'downloaded' in line:
-            p = downloaded.match(line).groupdict()
-            logger.info(f"{p['file']} 下载成功，下载位置：{p['path']}")
-        elif 'Downloading' in line:
-            logger.info(f"Downloading {line[12:-1]}")
-        elif line == "Failed to install browsers\n":
-            message = await shell.stdout.read()
-            logger.warning("Download Failed:\n" + message.decode("UTF-8"))
+        if not first_line:
+            await shell.wait()
+            # logger.info("Playwright 已经下载了哦")
+            return
+        elif "Downloading" not in first_line:
+            logger.warning("Playwright 下载停止")
+            logger.error(f"下载程序回报意外的内容:\n{first_line}")
+            shell.kill()
+            return
+
+        while line := (await shell.stdout.readline()).decode('UTF-8'):
+            percent = select.findall(line)
+            if percent and (time.time() - last_progress_time > 1 or percent[0] == "100"):
+                logger.info(f"Downloading {percent[0]}%")
+                last_progress_time = time.time()
+            elif 'downloaded' in line:
+                if (p := downloaded.match(line)) is not None:
+                    p = p.groupdict()
+                    logger.info(f"{p['file']} 下载成功，下载位置：{p['path']}")
+            elif 'Downloading' in line:
+                logger.info(f"Downloading {line[12:-1]}")
+            elif line == "Failed to install browsers\n":
+                message = await shell.stdout.read()
+                logger.warning("Download Failed:\n" + message.decode("UTF-8"))
 
     if shell.returncode:
-        logger.warning("下载失败，请重新启动，或者其他")
+        logger.warning(f"Playwright 下载失败，退出代码【{shell.returncode}】，请尝试重新运行，或按照官方教程手动下载")
+        logger.warning("Playwright 文档地址: https://playwright.dev/python/docs/intro")
+        logger.warning("使用命令 poetry run playwright install 或 pdm run playwright install 可手动下载 Playwright")
     else:
-        logger.success("下载成功")
+        logger.success("Playwright 下载成功")
