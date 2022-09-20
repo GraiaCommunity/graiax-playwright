a7b59f02141f6dc28de520ef0130ad91c54957f6
Redlnn <w731347477@gmail.com>
2022-09-20 22:27:15 +0800

    fix: 缺失的entry

--- src/graiax/playwright/__init__.py
+++ src/graiax/playwright/__init__.py
@@ -1,2 +1,3 @@
 from .interface import PlaywrightBrowser as PlaywrightBrowser
+from .interface import PlaywrightContext as PlaywrightContext
 from .service import PlaywrightService as PlaywrightService
